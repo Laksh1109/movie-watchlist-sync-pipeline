cc0b40a785af9f2d90fee4c3f6d08ee678934266
Laksh1109 <varshneylaksh1@gmail.com>
2026-04-21 21:18:05 +0530

    Refactor database functions in storage.py
    Removed SQL table creation and insertion logic.

--- storage.py
+++ storage.py
@@ -1,43 +1,23 @@
 import sqlite3
 import os
-
 DB_NAME = "movies.db"
-
 def init_db():
     print("🔥 init_db called")
     print("📁 Creating DB at:", os.path.abspath(DB_NAME))
-
     conn = sqlite3.connect(DB_NAME)
     cur = conn.cursor()
-
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS movies (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT,
-        rating REAL,
-        genre TEXT,
-        runtime INTEGER,
-        year INTEGER,
-        watched INTEGER DEFAULT 0
-    )
-    """)
-
+    cur.execute
     conn.commit()
     conn.close()
-
 
 def insert_movies(movies):
     print("🔥 insert_movies called")
     print("Number of movies:", len(movies))
-
     conn = sqlite3.connect(DB_NAME)
     cur = conn.cursor()
 
     for m in movies:
-        cur.execute("""
-        INSERT INTO movies (title, rating, genre, runtime, year)
-        VALUES (?, ?, ?, ?, ?)
-        """, (
+        cur.execute(, (
             m['title'],
             m['rating'],
             m['genre'],
