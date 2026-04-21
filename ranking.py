65a6044de7f638a4c0f8fcc6f6f95706abe8a602
Laksh1109 <varshneylaksh1@gmail.com>
2026-04-21 21:13:41 +0530

    Refactor rank_movies function for clarity

--- ranking.py
+++ ranking.py
@@ -1,19 +1,13 @@
 def rank_movies(movies):
-
-    # 🔥 filter unwatched movies
     movies = [m for m in movies if m.get("watched", 0) == 0]
-
     ranked = []
 
     for m in movies:
         rating = m['rating']
         runtime = m['runtime']
-
         score = rating + (1 / runtime if runtime else 0)
-
         m['score'] = score
         ranked.append(m)
-
     ranked.sort(key=lambda x: x['score'], reverse=True)
 
     return ranked
