595ddf2bfd0602967bb870fe396848cd529c84e5
Laksh1109 <varshneylaksh1@gmail.com>
2026-04-21 21:16:10 +0530

    Update api_client.py

--- api_client.py
+++ api_client.py
@@ -1,43 +1,31 @@
 import requests
-
 API_KEY = "7989f01f"
-
 def fetch_movie_data(title, year=None):
     title = str(title).replace(" ", "+")
-
     url = f"http://www.omdbapi.com/?t={title}&apikey={API_KEY}"
-
     if year:
         url += f"&y={year}"
-
     try:
         response = requests.get(url)
         data = response.json()
 
         if data.get("Response") == "False":
             return None
-
         return data
-
     except Exception as e:
         print("Error:", e)
         return None
 
-
 def search_movies(title):
     title = str(title).replace(" ", "+")
-
     url = f"http://www.omdbapi.com/?s={title}&apikey={API_KEY}"
-
     try:
         response = requests.get(url)
         data = response.json()
-
+        
         if data.get("Response") == "False":
             return []
-
         return data.get("Search", [])
-
     except Exception as e:
         print("Error:", e)
         return []
