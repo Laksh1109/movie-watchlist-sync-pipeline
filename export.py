adc4fb6641ccb2b3f4631e97843aa6e66047a396
Laksh1109 <varshneylaksh1@gmail.com>
2026-04-23 10:16:33 +0530

    Delete export.py

--- export.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def export_playlist(movies):
-    df = pd.DataFrame(movies)
-    df.to_csv("Weekend_Playlist.csv", index=False)
-    print("📄 Playlist exported to Weekend_Playlist.csv")
